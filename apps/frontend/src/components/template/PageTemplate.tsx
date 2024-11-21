517322bc6509e1ccba68fe83aa698fbcfad37fe1
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-21 09:47:51 -0300

    adicao do template das pagias na pagina principal.

--- apps/frontend/src/components/template/PageTemplate.tsx
+++ apps/frontend/src/components/template/PageTemplate.tsx
@@ -6,8 +6,7 @@ export interface PageTemplateProps{
 
 export function PageTemplate(props: Readonly<PageTemplateProps>){
     return (
-       
-        <div className="flex flex-col items-center py-10 min-h-screen bg-[url('/background.png')] bg-cover">
+        <div className="flex flex-col items-center py-10 min-h-screen bg-[url('/background.png')] bg-cover bg-center">
             <main className={props.className}>
                 {props.children}
             </main>
