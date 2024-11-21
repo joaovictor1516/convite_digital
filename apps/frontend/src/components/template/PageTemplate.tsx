23c0974c119ef8e3a02e0b4888e06ba21cee3bc0
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-21 09:28:37 -0300

    criacao do template das paginas, este componente servira como base para as paginas.

---
+++ apps/frontend/src/components/template/PageTemplate.tsx
@@ -0,0 +1,16 @@
+export interface PageTemplateProps{
+    children: React.ReactNode;
+    className?: string;
+}
+
+
+export function PageTemplate(props: Readonly<PageTemplateProps>){
+    return (
+       
+        <div className="flex flex-col items-center py-10 min-h-screen bg-[url('/background.png')] bg-cover">
+            <main className={props.className}>
+                {props.children}
+            </main>
+        </div>
+    )
+}
