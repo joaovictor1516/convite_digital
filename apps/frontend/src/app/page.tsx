22750e1660c9e2b10bd1a7fc8b211dd6da1b7506
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-25 18:52:14 -0300

    alteracoes parciais na pagina principal.

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
 import { PageTemplate } from "@/components/template/PageTemplate";
+import { GenerateId } from "@/core";
+import { EventProps } from "@/core";
 
 export default function Home() {
+  const id = GenerateId.new();
+
+  const event: Partial<EventProps> =  {id: id};
+  console.log(event)
   return (
     <PageTemplate>
-      <div>Página</div>
+      <div></div>
     </PageTemplate>
   );
 }
