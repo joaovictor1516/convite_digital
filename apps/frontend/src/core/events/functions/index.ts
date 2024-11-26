064bcd7972078a789e3683483701e7046573448f
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-26 15:08:20 -0300

    exportacao das funcoes de criar evento e convidado vazios e de complementar convidado.

--- apps/frontend/src/core/events/functions/index.ts
+++ apps/frontend/src/core/events/functions/index.ts
@@ -2,5 +2,14 @@ import { checkEventInformations } from "./isEmptyEventInformations";
 import { checkGuestInformations } from "./isEmptyGuestInformations";
 import { complementaryEvent } from "./complementaryEvent";
 import { complementaryGuest } from "./coplementaryGuest";
+import { createEmptyEvent } from "./createEmptyEvent";
+import { createEmptyGuest } from "./createEmptyGuest";
 
-export {checkEventInformations, checkGuestInformations, complementaryEvent, complementaryGuest};
+export {
+    checkEventInformations, 
+    checkGuestInformations, 
+    complementaryEvent, 
+    complementaryGuest, 
+    createEmptyEvent,
+    createEmptyGuest
+};
--- apps/frontend/src/core/events/index.ts
+++ apps/frontend/src/core/events/index.ts
@@ -1,5 +1,15 @@
 import { GuestProps } from "./model/guests";
 import { EventProps } from "./model/events";
-import { checkEventInformations, checkGuestInformations, complementaryEvent } from "./functions";
+import { checkEventInformations, checkGuestInformations, complementaryEvent, complementaryGuest,  createEmptyEvent, createEmptyGuest} from "./functions";
 
-export {type GuestProps, type EventProps, checkEventInformations, checkGuestInformations, complementaryEvent};
+export {
+    type GuestProps, 
+    type EventProps, 
+    checkEventInformations, 
+    checkGuestInformations, 
+    complementaryEvent,
+    complementaryGuest,
+    createEmptyEvent,
+    createEmptyGuest
+
+};
