b8e7f7c7dea5dbcec06d01a6e426bf085ac20a54
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-26 15:01:51 -0300

    adicao da verificacao do convidado apenas ter convidados se confirmar a sua presenca.

--- apps/frontend/src/core/events/functions/coplementaryGuest.ts
+++ apps/frontend/src/core/events/functions/coplementaryGuest.ts
@@ -7,12 +7,16 @@ export function complementaryGuest(guestPartial: Partial<GuestProps>): GuestProp
         throw new Error(errorMessages.join('\n'));
     }
 
+    const amountInvitesMade = guestPartial.amountInvitesMade ?? 0
+
+    const hasCompanion = guestPartial.isConfirmed && guestPartial.hasCompanion && amountInvitesMade > 0;
+
     const guest: GuestProps = {
         ...guestPartial,
         id: guestPartial.id ?? GenerateId.new(),
-        isConfirmed: guestPartial.isConfirmed ?? false,
-        hasCompanion: guestPartial.hasCompanion ?? false,
-        amountInvitesMade: guestPartial.amountInvitesMade ?? null
+        isConfirmed: hasCompanion ?? false,
+        hasCompanion: hasCompanion ?? false,
+        amountInvitesMade: hasCompanion ? guestPartial.amountInvitesMade : null
     } as GuestProps;
 
     return guest;
