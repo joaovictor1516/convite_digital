71f593a64ed336043d35c86ab45a266be338cbbc
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-27 08:55:31 -0300

    correcao no fundo do pageTemplate.

--- apps/frontend/src/components/template/PageTemplate.tsx
+++ apps/frontend/src/components/template/PageTemplate.tsx
@@ -8,7 +8,7 @@ export interface PageTemplateProps{
 
 export function PageTemplate(props: Readonly<PageTemplateProps>){
     return (
-        <div className="flex flex-col items-center py-10 min-h-screen bg-[url('/background.png')] bg-cover bg-center containerTemplate">
+        <div className="flex flex-col items-center py-10 min-h-screen bg-[url('/background.png')] bg-cover">
             <header>
                 <LogoTemplate/>
             </header>
