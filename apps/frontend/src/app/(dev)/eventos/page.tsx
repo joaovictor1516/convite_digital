f1f41a98aaf191e6f680180f6390af969f70c620
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-27 10:24:18 -0300

    melhoria no desenvolvimento da pagina de testes de funcoes nos eventos.

--- apps/frontend/src/app/(dev)/eventos/page.tsx
+++ apps/frontend/src/app/(dev)/eventos/page.tsx
@@ -1,12 +1,21 @@
 import { events } from "@/core"
+import Image from "next/image"
 
 export default function Eventos(){
     return (
-        <div>
+        <div className="grid grid-cols-3 gap-5">
             {events.map((event) => (
-                <span key={event.id}>
-                    {event.title}
-                </span>
+                
+                <div key={event.id} className="flex flex-col w-96 bg-zinc-500 rounded-lg">
+
+                    <div className="relative w-96 h-44">
+                        <Image src={event.imageMain} alt={event.title} fill className="rounded-t-lg object-cover"/>
+                    </div>
+
+                    <span className="">
+                        {event.title}
+                    </span>
+                </div>
             ))}
         </div>
     )
