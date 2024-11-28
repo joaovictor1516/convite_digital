84c30861096ee4df5ca6cd87b01a168032941498
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-28 18:54:56 -0300

    desenvolvimento das funcoes da api do evento.

--- apps/backend/src/events/events.controller.ts
+++ apps/backend/src/events/events.controller.ts
@@ -1,4 +1,47 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
+import { DateEvent, EventProps, events, GenerateId } from "core";
 
 @Controller("events")
-export class EventsController {}
+export class EventsController {
+  @Get()
+  async findEvents() {
+    return events.map(this.deserialize);
+  }
+
+  @Get(":idOrAlias")
+  async findEvent(@Param("idOrAlias") idOrAlias: string) {
+    if (GenerateId.validate(idOrAlias)) {
+      return this.serialize(
+        events.find((event: EventProps) => event.id === idOrAlias),
+      );
+    } else {
+      return this.serialize(
+        events.find((event: EventProps) => event.alias === idOrAlias),
+      );
+    }
+  }
+
+  @Get("validate/:alias/:id")
+  async validateAlias(@Param("alias") alias: string, @Param("id") id: string) {
+    const event = events.find((event: EventProps) => event.alias === alias);
+    return { valid: !event || event.id === id };
+  }
+
+  private serialize(event: EventProps) {
+    if (!event) return "event not found";
+
+    return {
+      ...event,
+      date: DateEvent.format(event.date),
+    };
+  }
+
+  private deserialize(event: any) {
+    if (!event) return "event not found";
+
+    return {
+      ...event,
+      date: DateEvent.unformat(event.date),
+    } as EventProps[];
+  }
+}
