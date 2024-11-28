0dad2c1b5e7e99db5dc9a3e71cac32a39549f4a9
joaovictor1516 <voliveira.joao30@sempreceub.com>
2024-11-27 22:10:30 -0300

    adicao do flex que estava faltando.

--- apps/frontend/src/components/template/PageTemplate.tsx
+++ apps/frontend/src/components/template/PageTemplate.tsx
@@ -12,7 +12,7 @@ export function PageTemplate(props: Readonly<PageTemplateProps>){
             <header>
                 <LogoTemplate/>
             </header>
-            <main className={`flex-1 flex-col justify-center py-10 container ${props.className}`}>
+            <main className={`flex-1 flex flex-col justify-center py-10 container ${props.className}`}>
                 {props.children}
             </main>
         </div>
